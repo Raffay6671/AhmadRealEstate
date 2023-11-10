a7bf3af0a38749a8854046d99bd61fb4e33939a2
Muhammad Sheharyar <msheharyarqazi@gmail.com>
2023-11-10 09:20:52 +0500

    footer UI done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import "../Styles/Footer.css"
 import quinta from "../Images/QuintaCodeBlack.png"
+import insta from "../Images/insta-icon.png"
+import facebook from "../Images/facebook-icon.png"
+import linkedin from "../Images/linkedin-icon.png"
+
 
 export default class Footer extends Component {
   render() {
@@ -9,9 +13,100 @@ export default class Footer extends Component {
         <div className="text-area">
                 <a href="#" className="logo">
                         <p className='name'>ahmadrealestate</p>
-                        <p className='com'>.com</p>
+                        <p className='comm'>.com</p>
                 </a>
-                <img className='quinta' src={quinta}/>
+                <a  target="_blank" href='https://www.linkedin.com/company/100022422/' className="quinta-block">
+                        <p className="developed">
+                                Developed by
+                        </p>
+                        <img className='quinta' src={quinta}/>
+                </a>
+               
+        </div>
+
+        <div className="link">
+                <div className="company">
+                        <a href='#' className='heading'>
+                                Company
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Contact us
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                About us
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Our Projects
+                        </a>
+                    
+                       
+
+                </div>
+                <div className="ser-cov">
+                        <div className="company">
+                        <a href='#' className='heading'>
+                                Services
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Properties for Sale
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Properties for Rent
+                        </a>
+
+                        </div>
+                        <div className="company">
+                        <a href='#' className='heading'>
+                                Coverage
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Islamabad
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Rawalpindi
+                        </a>
+                        <a href='#' className='sub-heading'>
+                                Murree
+                        </a>
+                        </div>
+                </div>
+                <div className="company">
+                <p className='heading'>
+                                Our Office
+                        </p >
+                        <p className='sub-heading'>
+                        Shop no 10, Blue Area, <br></br> Islamabad Capital Territory,<br></br> Pakistan
+                        </p>
+                        <p className='sub-heading'>
+                        Phone: 03357878678
+                        </p>
+                        <p className='sub-heading'>
+                        ahmadrealestate@gmail.com
+                        </p>
+
+                </div>
+                <div className="company">
+                        <p className='heading'>
+                                Follow us
+                        </p>
+                        <div className="social">
+                                <a>
+                                        <img href='#' className='icon' src={insta}/>
+
+                                </a>
+                                <a>
+                                        <img href='#' className='icon' src={facebook}/>
+
+                                </a>
+                                <a>
+                                        <img href='#' className='icon' src={linkedin}/>
+
+                                </a>
+                                
+                        </div>
+                        
+                </div>
+
         </div>
 
       </div>
