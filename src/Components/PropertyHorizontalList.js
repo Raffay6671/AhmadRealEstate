d0cfdbd2ce91550e6991fad26bbd597bd493bfe8
moizmazhar37 <moeez.ch3737@gmail.com>
2023-11-11 20:24:34 +0500

    dummy json made

--- src/Components/PropertyHorizontalList.js
+++ src/Components/PropertyHorizontalList.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Property from './Property'
 import "../Styles/propertyHorizontalList.css"
+import datajson from "../dataa.json"
 
 export default function PropertyHorizontalList(props) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const response = 
+        const jsonData = datajson;
+        await setData(jsonData);
+        console.log(data);  
+        // console.log(jsonData);
+      } catch (error) {
+        console.error('Errorr fetching JSON:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
         <div>   
-                <h className="propertyListHeading">Properties for {props.type}</h>
+                <p className="propertyListHeading">Properties for {props.type}</p>
             
                 
+              
+
                 <div className='propertyListContainer'>
-                <Property/>
-                <Property/>
-                <Property/>
-                <Property/>
+                  <Property/>
+                  <Property/>
+                  <Property/>
+                  <Property/>
                 </div>
         </div>
   )
